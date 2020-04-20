87df4b00b42999d284d7c56d0d81791edd1e5bf9
Ikenna Laurence <akaigbokwelaurence@gmail.com>
2020-04-20 11:24:22 +0100

    first commit

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <app-header></app-header>
+      <div>
+       <app-body></app-body>
+      </div>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninja from './components/Ninjas.vue';
+
+export default {
+  components:{
+    'app-header':Header,
+    'app-footer':Footer,
+    'app-body': Ninja
+  },
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+
+<style>
+
+</style>
