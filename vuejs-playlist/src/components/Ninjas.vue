6bfe6e89fbf574c92bd65491e6be0aadf1a27133
Ikenna Laurence <akaigbokwelaurence@gmail.com>
2020-04-21 07:43:24 +0100

    events, child to parent

--- vuejs-playlist/src/components/Ninjas.vue
+++ vuejs-playlist/src/components/Ninjas.vue
@@ -1,53 +1,53 @@
 <template>
-    <div id="ninja">
+    <div id="ninjas">
         <ul>
-            <li v-for="ninja in ninjas"
-            v-on:click="ninja.show = !ninja.show"
-            >
-               <h2>{{ninja.name}}</h2>  
-                <h3 v-show="ninja.show">{{ninja.speciality}}</h3> 
+            <li v-for="ninja in ninjas" v-on:click="ninja.show = !ninja.show">
+                <h2>{{ ninja.name }}</h2>
+                <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
             </li>
         </ul>
+        <button v-on:click="deleteNinja">Delete Ninja</button>
     </div>
 </template>
-
 <script>
 export default {
-  data() {
-    return {
-      ninjas: [
-        { name: "Ryu", speciality: "Vue Components", show: false },
-        { name: "Crystal", speciality: "HTML Wizardry", show: false },
-        { name: "Hitoshi", speciality: "Click Events", show: false },
-        { name: "Tango", speciality: "Conditionals", show: false },
-        { name: "Kami", speciality: "Webpack", show: false },
-        { name: "Yoshi", speciality: "Data Diggin", show: false }
-      ]
-    };
-  }
-};
+    props: {
+      ninjas: {
+        type: Array,
+        required: true
+      }
+    },
+    data(){
+        return{
+        }
+    },
+    methods: {
+      deleteNinja: function(){
+        this.ninjas.pop();
+      }
+    }
+}
 </script>
-
 <style scoped>
-#ninjas {
-  width: 100%;
-  max-width: 1200px;
-  margin: 40px auto;
-  padding: 0 20px;
-  box-sizing: border-box;
+#ninjas{
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
 }
-ul {
-  display: flex;
-  flex-wrap: wrap;
-  list-style-type: none;
-  padding: 0;
+ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
 }
-li {
-  flex-grow: 1;
-  flex-basis: 300px;
-  text-align: center;
-  padding: 30px;
-  border: 1px solid #222;
-  margin: 10px;
+li{
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
 }
 </style>
